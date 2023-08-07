2ae5ba32934150d2f7f513ab79e1e1729c3aa6c8
Rakesh Joshi <irakeshjoshi@live.com>
2023-08-07 21:43:50 +0530

    feat(implement-Omit): add solution for 'implement-Omit<T, K>' problem

---
+++ easy/006-implement-omit/solution.ts
@@ -0,0 +1,27 @@
+/**
+ * The aim of this challenge is to implement TypeScript's built-in `Omit<T, K>` type.
+ * The Omit type lets you create a new type by picking all properties from a given type `T` and then removing `K`.
+ *
+ * Here is the approach we'll take:
+ *
+ * - We'll use TypeScript's built-in utility type `Exclude` to get a type that represents all keys of `T` excluding keys in `K`.
+ * - We'll use a mapped type to iterate over each property in `T`. For each property `P` that is in the set of keys returned by `Exclude<keyof T, K>`, we'll assign the original type of that property from `T` to the new type.
+ * 
+ * By using this approach, we obtain a new type with all the properties from `T`, excluding those specified in `K`.
+ */
+
+type MyOmit<T, K extends keyof any> = {
+    [P in Exclude<keyof T, K>]: T[P];
+}
+
+// Testing our implementation
+
+type Foo = {
+    a: string
+    b: number
+    c: boolean
+}
+
+type A = MyOmit<Foo, 'a' | 'b'>; // Expected: {c: boolean}
+type B = MyOmit<Foo, 'c'>; // Expected: {a: string, b: number}
+type C = MyOmit<Foo, 'c' | 'd'>; // Expected: {a: string, b: number}
